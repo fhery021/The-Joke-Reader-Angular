78806c55aac41237e252104f7b93cee9b30d1d2c
Ferenc Bondar <fhery021@gmail.com>
2019-08-21 11:51:59 +0300

    npm audit

--- TheJokeReader/src/app/app.module.ts
+++ TheJokeReader/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TheJokeReader/src/app/home/home.component.ts
+++ TheJokeReader/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface DataResponse {
+  id: number;
+  joke: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  showComments = false;
+  btnTextShowComments = 'Comments';
+  btnTextHideComments = 'Hide Comments';
+  btnCommentsText: string = this.btnTextShowComments;
+
+  public jokes: DataResponse[] = [];
+
+  // TODO log in feature
+  loggedIn = true;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://192.168.1.7:8080/jokes/getRandomJokes/25')
+    .subscribe(data =>{
+      for(var i=0; i<25; i++){
+        this.jokes.push(data[i] as DataResponse);
+      }
+    });
+  }
+
+  toggle(): void {
+    if (this.showComments) {
+      this.btnCommentsText = this.btnTextHideComments;
+      this.showComments = false;
+    } else {
+      this.btnCommentsText = this.btnTextShowComments;
+      this.showComments = true;
+    }
   }
 
 }
